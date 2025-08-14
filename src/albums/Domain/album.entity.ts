cda29eb3d16ae72fa94a8661c00f13c3da2e38c5
alejandroMonteroAssure <alejandro.montero@assuresoft.tech>
2025-08-14 12:56:13 -0400

    Done

---
+++ src/albums/Domain/album.entity.ts
@@ -0,0 +1,7 @@
+export interface IArtist{
+    artist_id?: number;
+    name: string;
+    profile_img: string | null;
+    debut_date: Date;
+}
+//https://excalidraw.com/#json=x1_CrivDXFpxo8UNJ-dzj,ASvqi3NJ01BLmdBYFAamsg
